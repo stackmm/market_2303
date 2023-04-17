c303b637a8f665f8f6efba5bd1ec59a4de0729d9
Steven Stackhouse <stackmm@gmail.com>
2023-04-17 10:08:06 -0600

    add vendor_names tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -36,4 +36,14 @@ RSpec.describe Market do
       expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
     end
   end
+
+  describe "#vendor_names" do
+    it "can list the names of all vendors in the market" do
+      expect(@market.vendor_names).to eq([])
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+    end
+  end
 end
