1c02a85a33fdf360a9d8aacafcbab3b3ef53f51b
Steven Stackhouse <stackmm@gmail.com>
2023-04-17 09:36:35 -0600

    Add stock tests

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -25,4 +25,17 @@ RSpec.describe Vendor do
     end
   end
 
+  describe "#stock" do
+    it "can stock an item" do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+      @vendor.stock(@item1, 30)
+      expect(@vendor.check_stock(@item1)).to eq(30)
+      @vendor.stock(@item1, 25)
+      expect(@vendor.check_stock(@item1)).to eq(55)
+      @vendor.stock(@item2, 12)
+      expect(@vendor.check_stock(@item2)).to eq(12)
+      expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
+    end
+  end
+
 end
