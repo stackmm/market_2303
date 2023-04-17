b317770497f1e4be9db98e0d99b6c94a207ec89e
Steven Stackhouse <stackmm@gmail.com>
2023-04-17 10:19:13 -0600

    add vendors_that_sell tests

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -46,4 +46,25 @@ RSpec.describe Market do
       expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
     end
   end
+
+  describe "#vendors_that_sell" do
+    it "can list the vendors that have an item in stock" do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.vendors_that_sell(@item1)).to eq([])
+      expect(@market.vendors_that_sell(@item2)).to eq([])
+      expect(@market.vendors_that_sell(@item3)).to eq([])
+      expect(@market.vendors_that_sell(@item4)).to eq([])
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      @vendor3.stock(@item1, 65)
+      expect(@market.vendors_that_sell(@item1)).to eq([@vendor1, @vendor3])
+      expect(@market.vendors_that_sell(@item2)).to eq([@vendor1])
+      expect(@market.vendors_that_sell(@item3)).to eq([@vendor2])
+      expect(@market.vendors_that_sell(@item4)).to eq([@vendor2])
+    end
+  end
 end
